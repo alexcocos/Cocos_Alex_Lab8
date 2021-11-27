548b1b50c4643972cbb085b750b002434130f31a
Alex <alexcocos46@gmail.com>
2021-11-27 17:29:28 +0200

    Add project files.

---
+++ Data/Cocos_Alex_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cocos_Alex_Lab8.Models;
+
+namespace Cocos_Alex_Lab8.Data
+{
+    public class Cocos_Alex_Lab8Context : DbContext
+    {
+        public Cocos_Alex_Lab8Context (DbContextOptions<Cocos_Alex_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cocos_Alex_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Cocos_Alex_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cocos_Alex_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cocos_Alex_Lab8.Data;
+using Cocos_Alex_Lab8.Models;
+
+namespace Cocos_Alex_Lab8.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Cocos_Alex_Lab8.Data.Cocos_Alex_Lab8Context _context;
+
+        public DeleteModel(Cocos_Alex_Lab8.Data.Cocos_Alex_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
